f05090bdfddaa2130bca87f0c5d58cef1b59ea4e
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2021-08-23 23:31:37 +0530

    commit

--- src/db/connection.js
+++ src/db/connection.js
@@ -2,13 +2,16 @@ const mongoose = require("mongoose");
 const chalk = require("chalk");
 
 mongoose
-  .connect("", {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  })
+  .connect(
+    "",
+    {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
   .then(() => {
     console.log(chalk.greenBright.cyan("Database connection successful"));
   })
